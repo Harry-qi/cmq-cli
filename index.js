ca46fa9e2ac2a2bc480691e9c2baf0a260f3ed36
chenmingqi <674816588@qq.com>
2021-09-20 11:31:36 +0800

    feat:优化代码

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ import { program } from 'commander';   // 命令行
 import inquirer from 'inquirer';       // 交互式的命令
 import download from "download-git-repo";  // 下载并提取 git 仓库，用于下载项目模板
 import ora from 'ora';
-
 import chalk from "chalk"; // 给终端的字体加上颜色
 
 import path from 'path';
 import fs from 'fs';
 import child_process from 'child_process';
 
-import templates from './template/index.js'
+import downloadList from './template/index.js'
 
+const templates = Object.keys(downloadList)
 // 命令行选择列表
 let promptList = [
   {
@@ -58,12 +58,12 @@ function downloadTemplate(gitUrl, projectName) {
 // 下载远程仓库
 async function downloadGitTemplate(projectName, gitUrl) {
   console.log(
-      chalk.bold.cyan("cmq_cli: ") + "creating..."
+    chalk.bold.cyan("cmq_cli: ") + "creating..."
   );
   try {
     await checkName(projectName); // 检查项目名
     await downloadTemplate(gitUrl, projectName) // 下载模板
-    await setPackageContent(projectName);
+    await setPackageContent(projectName); // 设置package.json的name
     installDependencies(projectName)  // 安装依赖
   } catch (error) {
     console.log(chalk.red(error));
@@ -71,36 +71,22 @@ async function downloadGitTemplate(projectName, gitUrl) {
 }
 // 安装依赖
 function installDependencies(filename) {
-    console.log("Dependencies are being installed, it may take a few minutes");
-    const spinner = ora("Dependencies downloading...",'Box1').start();
-    child_process.exec('npm i',{cwd: path.resolve(process.cwd(), filename)}, (error)=>{
-      if(error){
-        console.error(error)
-        return
-      }
-      spinner.stop();
-      console.log(
-        chalk.bold.cyan("cmq_cli: ") + "finished!"
-      );
-    })
+  console.log("Dependencies are being installed, it may take a few minutes");
+  const spinner = ora("Dependencies downloading...",'Box1').start();
+  child_process.exec('npm i',{cwd: path.resolve(process.cwd(), filename)}, (error)=>{
+    if(error){
+      console.error(error)
+      return
+    }
+    spinner.stop();
+    console.log(
+      chalk.bold.cyan("cmq_cli: ") + "finished!"
+    );
+  })
 
 }
 // 选择以自定义模板还是git的模板
 function checkTem(template,filename){
-  let downloadList = {
-      "vant-mobile-template": {
-        src: "direct:https://github.com/Harry-qi/vant-mobile-template.git"
-      },
-      "vue2-template": {
-        src: "direct:https://github.com/Harry-qi/vue2-tempalte.git"
-      },
-      "vue2-simple-admin": {
-        src: "direct:https://github.com/Harry-qi/vue-admin-template.git"
-      },
-      "vue-element-admin": {
-        src: "direct:https://github.com/PanJiaChen/vue-element-admin.git"
-      }
-  }
   downloadGitTemplate(filename,downloadList[template].src)
 }
 // 设置package.json的一些信息
@@ -134,13 +120,13 @@ async function setPackageContent(projectName) {
 
 // 设置选项
 program
-    .command('create <filename>')
-    .description('选择模板项目')
-    .action(async (filename) => {
-      const res = await inquirer.prompt(promptList)  // 命令行选择的选择的选项
-      console.log("选择了", res.template);
-      checkTem(res.template,filename)
-    })
+  .command('create <filename>')
+  .description('选择模板项目')
+  .action(async (filename) => {
+    const res = await inquirer.prompt(promptList)  // 命令行选择的选择的选项
+    console.log("选择了", res.template);
+    checkTem(res.template,filename)
+  })
 
 // cli版本
 const packageData = fs.readFileSync('./package.json', 'utf8')
--- template/index.js
+++ template/index.js
@@ -1,10 +1,16 @@
 // git下载模板
-let downloadList = [
-  "vant-mobile-template",
-  "vue2-template",
-  "vue2-simple-admin",
-  "vue-element-admin"
-]
-export default  [
-    ...downloadList
-]
+let downloadList = {
+  "vant-mobile-template": {
+    src: "direct:https://github.com/Harry-qi/vant-mobile-template.git"
+  },
+  "vue2-template": {
+    src: "direct:https://github.com/Harry-qi/vue2-tempalte.git"
+  },
+  "vue2-simple-admin": {
+    src: "direct:https://github.com/Harry-qi/vue-admin-template.git"
+  },
+  "vue-element-admin": {
+    src: "direct:https://github.com/PanJiaChen/vue-element-admin.git"
+  }
+}
+export default  downloadList
